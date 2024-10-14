e4ef354b6872d30db164da6758dee887d735415e
PorusDwinner <amit2811995@gmail.com>
2024-10-14 16:51:21 +0530

    login route complete

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,10 @@
 const User = require('../models/userModal');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const expiry = 7 * 24 * 60 * 60 * 1000;
 const { NResposne } = require('../responses/response');
+require('dotenv').config();
+const jwtKey = process.env.JWT_SECRET;
 
 module.exports.signup_post = async(req, res) => {
     try{
@@ -32,8 +35,61 @@ module.exports.signup_post = async(req, res) => {
 };
 
 module.exports.login_post = async(req, res) => {
-    
-}
+    try{
+        const { email, password } = req.body;
+        if(!email  || !password){
+            return res.status(400).json(new NResposne(
+                0,
+                "Wrong credentials",
+                '',
+                []
+            ));
+        }
+
+        const user = await User.findOne({ email });
+        if(!user){
+            return res.status(401).json(new NResposne(
+                0,
+                "No such user found",
+                '',
+                []
+            ));
+        };
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(401).json(new NResposne(
+                0,
+                "Wrong password",
+                '',
+                []
+            ))
+        };
+
+        const token = jwt.sign(
+            { id: user._id, email: user.email }, 
+            jwtKey, 
+            { expiresIn: expiry }
+        );
+
+        return res.status(200).json(new NResposne(
+
+            1,
+            "Login Succesfull",
+            token,
+            user
+        ));
+    }
+    catch (err){
+        console.error(err);
+        return res.status(500).json(new NResposne(
+            0,
+            'Internal server error',
+            '',
+            []
+        ));
+    }
+};
 
 module.exports.signup_get = async(req, res) => {
     res.status(200).json("Signup Page")
@@ -62,7 +118,7 @@ function handleErrors(err){
 function createToken(id){
     return jwt.sign(
         { id },
-        'jwtsecretkey',
+        jwtKey,
         { expiresIn : 3*24*60*60*1000 }
     )
 };
