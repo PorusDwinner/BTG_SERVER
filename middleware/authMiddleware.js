3dddf7e61d18edfad8b4c0aa3680165ab9b8028c
Amit Saini <112637683+PorusDwinner@users.noreply.github.com>
2024-10-15 14:47:08 +0530

    minor changes in auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,19 +1,32 @@
 const jwt = require('jsonwebtoken');
+const { NResposne } = require('../responses/response');
 require('dotenv').config();
 const jwtKey = process.env.JWT_SECRET;
 
 const isAuthorized = (req, res, next) => {
     const authToken = req.cookie.token;
-    if(authToken){
+    if (authToken) {
         jwt.verify(authToken, jwtKey, (err, decodedToken) => {
-            if(err){
+            if (err) {
                 console.log(err.message);
-                return false;
+                return res.status(401).json(new NResposne(
+                    0,
+                    "Authorization failed, please login again",
+                    "",
+                    []
+                ))
             } else {
                 next();
             }
         });
-    } else return false;
+    } else {
+        return res.status(401).json(new NResposne(
+            0,
+            "Authorization failed, please login again",
+            "",
+            []
+        ))
+    };
 };
 
 module.exports = { isAuthorized };
