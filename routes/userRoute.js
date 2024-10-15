0588b886b2b1d90a4b63ee24261a33e6c0788076
Amit Saini <112637683+PorusDwinner@users.noreply.github.com>
2024-10-15 17:05:56 +0530

    get dp api added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,5 +5,7 @@ const controller = require('../controllers/userController');
 
 router.get('/get/profile', isAuthorized, controller.get_dp);
 router.put('/update/profile', isAuthorized, controller.update_profile);
+router.put('/update/dp', isAuthorized, controller.update_dp);
+router.post('/post/dp', isAuthorized, controller.post_dp);
 
 module.exports = router;
